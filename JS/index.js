fb4b93192cd46f5bdeef9c2192fbbb915bf05746
Ravindra Malviya <32036542+malviyaravi@users.noreply.github.com>
2021-11-15 22:22:30 +0530

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+// document.getElementById("button").onclick = function() {
+//          alert('hello');
+//  }
+var x = document.getElementsByClassName("pnblock");
+
+// var y = x%1000;
+ function fun() {
+   
+// var a = prompt(y, " ");
+// while (a != y) {
+//     a = prompt("Renter OTP", " ");
+// }
+ }
